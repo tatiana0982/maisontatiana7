f78d2dd173f14b4a193a5cb68d5b0223333eecfb
Dev Jadiya <devjadiya15@gmail.com>
2025-08-19 19:29:58 +0530

    updated

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -178,7 +178,6 @@ const ProductDetailPage = ({ params }: { params: { slug: string } }) => {
                   <button onClick={() => setQuantity(q => q + 1)} className="px-4 py-3 text-lg transition-colors hover:bg-gray-100">+</button>
                 </div>
                 <Button size="lg" className="flex-grow bg-black text-white hover:bg-gray-800 flex items-center justify-center gap-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" /></svg>
                   Add to Cart
                 </Button>
               </div>
