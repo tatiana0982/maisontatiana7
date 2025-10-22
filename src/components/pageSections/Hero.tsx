79c8a74129e06c7ea4ab5ed197e07cb3159161ec
Dev Jadiya <devjadiya15@gmail.com>
2025-10-22 17:02:03 +0530

    updated

--- src/components/pageSections/Hero.tsx
+++ src/components/pageSections/Hero.tsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Hero: React.FC = () => {
-  const [imageSrc, setImageSrc] = useState('/images/heroimg.jpg'); // Image source for both screen sizes
-
-  // Effect to listen to window resizing
-  useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 767px)'); // Example for small screen size
-
-    const handleResize = (e: MediaQueryListEvent | MediaQueryList) => {
-      setImageSrc('/images/heroimg.jpg'); // Always use the same image
-    };
-
-    // Set the initial image source
-    handleResize(mediaQuery);
-
-    // Listen for changes in screen size
-    mediaQuery.addEventListener('change', handleResize);
-
-    // Cleanup listener on component unmount
-    return () => mediaQuery.removeEventListener('change', handleResize);
-  }, []);
+  const imageSrc = '/images/heroimg.png';
 
   return (
-    <section className="relative min-h-screen w-full overflow-hidden bg-white">
-      {/* Image Display */}
+    <section className="w-full overflow-hidden bg-white pt-24">
       <img
         src={imageSrc}
         alt="Hero Image"
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        className="w-full h-auto"
       />
     </section>
   );
